409b5ebd2f1f31c8ab705333ae07410e4281170d
Shuanghai Wang <liuchengcheng@liuchengchengdeiMac.local>
2018-10-29 13:36:37 +0800

    add class_struct test pro

---
+++ classStruct/classStruct/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  classStruct
+//
+//  Created by WangShuanghai on 2018/10/29.
+//  Copyright © 2018 双海. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
